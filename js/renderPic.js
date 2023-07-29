ce7679c587bc42812aae1ee175d17cd83790e41e
Qstimo <vadtolpekin@gmail.com>
2023-07-29 17:05:27 +0300

    render

---
+++ js/renderPic.js
@@ -0,0 +1,4 @@
+let template = document.querySelector('#picture').content;
+let likes = template.querySelector('.picture__likes');
+let comments = template.querySelector('.picture__comments');
+let fragment = document.createDocumentFragment();
